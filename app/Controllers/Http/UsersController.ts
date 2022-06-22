d5c3f36fbb935db58d3b736ca2395a36425c8fc7
Lucas Cipriano <lucasncborges@gmail.com>
2022-06-22 14:46:27 -0300

    Aula 18, atualizado teste e está devolvendo corretamente o objeto enviadoo

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,7 +1,11 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
 
 export default class UsersController {
-  public async store({ response }: HttpContextContract) {
-    return response.created({})
+  public async store({ request, response }: HttpContextContract) {
+    const userPayload = request.only(['username', 'email', 'password', 'avatar'])
+    const user = await User.create(userPayload)
+
+    return response.created({ user })
   }
 }
--- tests/functional/users/users.spec.ts
+++ tests/functional/users/users.spec.ts
@@ -1,7 +1,13 @@
+import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 
-test.group('User', () => {
-  test('Criação do usuário', async ({ client }) => {
+test.group('User', (group) => {
+  // Iniciar migration e no final deletar, para não dar ruim
+  group.each.setup(async () => {
+    await Database.beginGlobalTransaction()
+    return () => Database.rollbackGlobalTransaction()
+  })
+  test('Criação do usuário', async ({ client, assert }) => {
     const userPayload = {
       email: 'test@test.com',
       username: 'test',
@@ -11,8 +17,9 @@ test.group('User', () => {
     const response = await client.post('/users').json(userPayload)
     const { password, avatar, ...expected } = userPayload
 
-    response.assertBodyContains(expected)
+    response.assertBodyContains({ user: expected })
 
     response.assertStatus(201)
+    assert.exists(response.body().user.password, 'existe')
   })
 })
