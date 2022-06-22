fb59a64a0b39cc09e75b204017aa920fa47f71ad
Lucas Cipriano <lucasncborges@gmail.com>
2022-06-22 15:33:00 -0300

    email já em uso

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,9 +1,12 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import BadRequestException from 'App/Exceptions/BadRequestException'
 import User from 'App/Models/User'
 
 export default class UsersController {
   public async store({ request, response }: HttpContextContract) {
     const userPayload = request.only(['username', 'email', 'password', 'avatar'])
+    const userByemail = await User.findBy('email', userPayload.email)
+    if (userByemail) throw new BadRequestException('Email já em uso', 409)
     const user = await User.create(userPayload)
 
     return response.created({ user })
--- tests/functional/users/users.spec.ts
+++ tests/functional/users/users.spec.ts
@@ -1,5 +1,6 @@
 import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
+import { UserFactory } from 'Database/factories'
 
 test.group('User', (group) => {
   // Iniciar migration e no final deletar, para não dar ruim
@@ -21,5 +22,23 @@ test.group('User', (group) => {
 
     response.assertStatus(201)
     assert.notExists(response.body().user.password, 'Passowrd Defined')
+  })
+  test('Erro 409 caso email esteja em uso', async ({ client, assert }) => {
+    const { email } = await UserFactory.create()
+    const response = await client.post('/users').json({
+      username: 'test',
+      email,
+      password: 'password',
+      avatar: 'https://avatar.com/avatar/avatar',
+    })
+    console.log(response.response)
+    response.assertStatus(409)
+
+    assert.exists(response.body().message)
+    assert.exists(response.body().code)
+    assert.exists(response.body().status)
+    assert.exists(response.body().message, 'email')
+    assert.equal(response.body().code, 'BAD_REQUEST')
+    assert.equal(response.body().status, 409)
   })
 })
