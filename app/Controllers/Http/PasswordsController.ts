99c0111a16e64bad0b7369561f995ab467e16b26
Lucas Cipriano <lucasncborges@gmail.com>
2022-06-23 08:47:13 -0300

    teste envio de email, ainda com error na checagem do html

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -1,7 +1,16 @@
+import Mail from '@ioc:Adonis/Addons/Mail'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class PasswordsController {
-  public async forgotPassword({ response }: HttpContextContract) {
+  public async forgotPassword({ response, request }: HttpContextContract) {
+    const { email } = request.only(['email'])
+    await Mail.send((message) => {
+      message
+        .from('no-replay@apilucas.com')
+        .to(email)
+        .subject('Recuperação de senha')
+        .text('Clique no link a baixo para redefinir a senha')
+    })
     return response.noContent()
   }
 }
--- tests/functional/users/password.spec.ts
+++ tests/functional/users/password.spec.ts
@@ -1,19 +1,25 @@
 import Database from '@ioc:Adonis/Lucid/Database'
 import { test } from '@japa/runner'
 import { UserFactory } from 'Database/factories'
+import Mail from '@ioc:Adonis/Addons/Mail'
 test.group('Password', (group) => {
   // Iniciar migration e no final deletar, para não dar ruim
   group.each.setup(async () => {
     await Database.beginGlobalTransaction()
     return () => Database.rollbackGlobalTransaction()
   })
-  test('Envio de email para redefinir a senha', async ({ client }) => {
+  test('Envio de email para redefinir a senha', async ({ assert, client }) => {
     const user = await UserFactory.create()
+    const mailer = Mail.fake()
 
-    const response = await client.post('/forgot-password').json({
+    await client.post('/forgot-password').json({
       email: user.email,
       resetPasswordUrl: 'url',
     })
-    response.assertStatus(204)
+    assert.isTrue(mailer.exists({ to: [{ address: user.email }] }))
+    assert.isTrue(mailer.exists({ from: { address: 'no-replay@apilucas.com' } }))
+    assert.isTrue(mailer.exists({ subject: 'Recuperação de senha' }))
+    // assert.isTrue(mailer.exists((mail) => mail.html!.includes(user.username)))
+    Mail.restore()
   })
 })
